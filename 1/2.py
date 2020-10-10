40cc2f649548a6bf208b61807e97e78d4528436c
Andrey <tverscay6@gmail.com>
2020-10-10 10:25:52 +0300

    Initial commit

---
+++ 1/2.py
@@ -0,0 +1,4 @@
+a = float(input("первое число"))
+b = float(input("второе число"))
+print(a + b)
+print(a * b)
---
+++ 1/3.py
@@ -0,0 +1,7 @@
+ab = float(input("первое число"))
+bc = float(input("второе число"))
+dm = float(input("третье число"))
+one = ab * 2
+too = bc - 3
+three = dm ** 2
+print(one + too + three)
