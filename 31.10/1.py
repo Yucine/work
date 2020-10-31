6c4f1c0380fa46e5db02b183a4a588501c6c6076
Andrey <tverscay6@gmail.com>
2020-10-31 10:43:23 +0300

    Initial commit.

---
+++ 31.10/1.py
@@ -0,0 +1,15 @@
+import random
+seriy=0
+i=0
+b=0
+while i<=20:
+    while seriy < 12:
+        a=(random.randint(0,2))
+        seriy+=a
+        b =a
+        print(b)
+    if seriy == 12:
+        i+=1
+
+        
+    
